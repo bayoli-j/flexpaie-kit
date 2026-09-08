128fe1d9a4a0c084090841196cf098b5e2e368db
bayoli-j <bayoli.j@infoset.cd>
2026-09-08 13:11:31 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "FlexPaieKit",
-         url: "https://github.com/bayoli-j/flexpaie-kit/releases/download/1.5.2/FlexPaieKit.xcframework.zip",
-         checksum:"a82f08e8e0607c54fdb9e4147d9efec3067d6178e30976c8a605ac6287043901"
+         url: "https://github.com/bayoli-j/flexpaie-kit/releases/download/1.5.3/FlexPaieKit.xcframework.zip",
+         checksum:"e4832cf0e9f7b0847cdf7394aa7fa0c0394201a686a13abf624c6a8ef406d173"
       )
    ]
 )
